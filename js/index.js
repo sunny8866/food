31693ac116ef14dba9f775fb7d5acd66759c7ea2
pigpig12376 <834543384@qq.com>
2019-08-20 23:51:15 +0800

    index.js

--- js/index.js
+++ js/index.js
@@ -12,10 +12,11 @@ var ulImgs=document.getElementById("ul_imgs");
 // 包含小圆点列表的ul
 var ulIdxs=document.getElementById("ul_idxs");
 // 小圆点的元素列表
-var lis=ulIdexs.children;
+var lis=ulIdxs.children;
+// 移动的函数
 function moveTo(to){
     // 若用户没有传入值，执行默认操作
-    if(i==undefined){
+    if(to==undefined){
         to=i+1;
     }
     if(i==0){
@@ -32,7 +33,10 @@ function moveTo(to){
         }
     }
     i=to;
-    ulImgs.style.marginLeft=-i*liWidth+"px";
+    ulImgs.style.marginLeft=-i*liwidth+"px";
+    for(var li of lis){
+        li.className="";
+    }
     if(i==liCount){
         i=0;
         setTimeout(function(){
@@ -41,4 +45,55 @@ function moveTo(to){
         },duration);
     }
     lis[i].className="active";
+}
+// 用DOM查找左右按钮
+var btnLeft=document.getElementById("btn_left");
+var btnRight=document.getElementById("btn_right");
+// 开关
+var canClick=true;
+btnRight.onclick=function(){
+    moveTo(1);
+}
+// 左右按钮共用的移动函数
+function move(n){
+    if(canClick){
+        moveTo(i+n);
+        canClick=false;
+        setTimeout(function(){
+            canClick=true;
+        },duration+100);
+    }
+}
+var interval=3000;
+var timer=setInterval(function(){
+    moveTo();
+},3000);
+var banner=document.getElementById("banner");
+banner.onmouseover=function(){
+    clearInterval(timer);
+}
+banner.onmouseout=function(){
+    timer=setInterval(function(){
+        moveTo();
+    },3000);
+}
+// 小圆点的点击效果
+var ulIdxs=document.getElementById("ul_idxs");
+ulIdxs.onclick=function(e){
+    if(canClick){
+        var li=e.target;
+        if(li.nodeName=="LI"){
+            if(li.className!=="active"){
+                for(var i=0;i<lis.length;i++){
+                    if(lis[i]==li){
+                        break;
+                    }
+                }
+                moveTo(i);
+                setTimeout(function(){
+                    canClick=true;
+                },duration);
+            }
+        }
+    }
 }
